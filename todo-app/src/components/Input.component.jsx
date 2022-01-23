be2e1351d5a1650a9994c5d41d9676f75f56d3bd
Ikfreecharles <68482360+Ikfreecharles@users.noreply.github.com>
2022-01-23 22:36:58 +0100

    Revert "First project commit"

--- todo-app/src/components/Input.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ButtonComponent } from "./Button.component";
-import InputStyle from "../components/Input.style.css"
-
-export const InputComponent = () => {
-  return (
-  <input type="text" name="" placeholder="Add your new todo" />
-  );
-};
--- todo-app/src/components/List.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ListStyle from "../components/List.styles.css";
-export const ListComponent = (props) => {
-  return(
- 
-    <li>I will wash today</li>
-
-  )
-};
