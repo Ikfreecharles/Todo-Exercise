0eead21e897f7305caa33094aa01ff568c70aede
CaptainCoder <78704276+captainplanto@users.noreply.github.com>
2022-02-27 21:40:32 +0100

    Update Home.js

--- todo-app/src/pages/Home.js
+++ todo-app/src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import { ButtonComponent } from "../components/Button.component";
 import { InputComponent } from "../components/Input.component";
 import { ListComponent } from "../components/List.component";
-import "./Button.styles.css";
 
 
 export const Home = () => {
